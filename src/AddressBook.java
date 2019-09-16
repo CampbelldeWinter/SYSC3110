d22479f2daf2f310726e4e0933607ed1ee868e7e
campbelldewinter <campbelldewinter@cb5109-10.labs.sce.carleton.ca>
2019-09-16 11:33:35 -0400

    created a book and buddy. Added buddy to book and removed it

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,14 +3,19 @@ public class AddressBook {
 	private List<BuddyInfo> buddies;
 	
 	public void addBuddy(BuddyInfo buddy) {
-		buddies.add(buddy);
+		if (buddy != null) buddies.add(buddy);
 	}
 	
-	public void removeBuddy(BuddyInfo buddy) {
-		buddies.remove(buddy);
+	public void removeBuddy(int index) {
+		if (index >=0 && index < buddies.size()) buddies.remove(index);
+		
 	}
 	
 	public void main(String[] args) {
 		System.out.println("#AlwaysBeBurying");
+		BuddyInfo fren = new BuddyInfo();
+		AddressBook book = new AddressBook();
+		book.addBuddy(fren);
+		book.removeBuddy(0);
 	}
 }
